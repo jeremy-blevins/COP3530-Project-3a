d155ef505d6608a88a7190c27a9151fb1ed6fa5c
Jeremy Blevins <jeremy.blevins@me.com>
2023-04-23 12:33:53 -0700

    push retry

--- Songs.h
+++ Songs.h
@@ -1,8 +1,115 @@
-//
-// Created by Jeremy Blevins on 4/23/23.
-//
+#include <iostream>
+#include <vector>
 
-#ifndef COP3530_PROJECT_3A_SONGS_H
-#define COP3530_PROJECT_3A_SONGS_H
+class Songs {
+private:
+    std::vector<std::vector<std::string>> songList;
+    int FindIndex(std::string searchName);
 
-#endif //COP3530_PROJECT_3A_SONGS_H
+public:
+    void AddSong(std::vector<std::string> songInfo);
+    std::string GetAlbum(std::string searchName);
+    std::string GetArtists(std::string searchName);
+    double GetDanceability(std::string searchName);
+    double GetEnergy(std::string searchName);
+};
+
+int Songs::FindIndex(std::string searchName) {
+    //testing
+    std::cout << songList.size() << std::endl;
+
+    //set index to -1
+    int index = -1;
+
+    //search songList for song name
+    for (int i = 0; i < songList.size(); i++){
+        if (searchName == songList[i][1]){
+            index = i;
+        }
+    }
+    return index;
+}
+
+void Songs::AddSong(std::vector<std::string> songInfo) {
+    //temp variables;
+    std::string songID, songName, songAlbum, songArtists, songDanceability, songEnergy;
+
+    //get relevant song info
+    songID = songInfo[0];
+    songName = songInfo[1];
+    songAlbum = songInfo[2];
+    songArtists = songInfo[4];
+    songDanceability = songInfo[9];
+    songEnergy = songInfo[10];
+
+    //add song to songList
+    songList.push_back({songID, songName, songAlbum, songArtists, songDanceability, songEnergy});
+}
+
+std::string Songs::GetAlbum(std::string searchName) {
+    //temp variables
+    std::string result;
+    int index;
+
+    //find index of song
+    index = FindIndex(searchName);
+
+    if (index == -1){
+        result = "ERROR: Song not found";
+    } else {
+        result = songList[index][2];
+    }
+
+    return result;
+}
+
+std::string Songs::GetArtists(std::string searchName) {
+    //temp variables
+    std::string result;
+    int index;
+
+    //find index of song
+    index = FindIndex(searchName);
+
+    if (index == -1){
+        result = "ERROR: Song not found";
+    } else {
+        result = songList[index][3];
+    }
+
+    return result;
+}
+
+double Songs::GetDanceability(std::string searchName) {
+    //temp variables
+    double result;
+    int index;
+
+    //find index of song
+    index = FindIndex(searchName);
+
+    if (index == -1){
+        result = -1.0;
+    } else {
+        result = std::stod(songList[index][4]);
+    }
+
+    return result;
+}
+
+double Songs::GetEnergy(std::string searchName) {
+    //temp variables
+    double result;
+    int index;
+
+    //find index of song
+    index = FindIndex(searchName);
+
+    if (index == -1){
+        result = -1.0;
+    } else {
+        result = std::stod(songList[index][5]);
+    }
+
+    return result;
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,55 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include "Songs.h"
+
+void parseFile(const std::string& filename, Songs songList){
+    //create an input filestream
+    std::fstream file;
+
+    file.open(filename, std::ios::in);
+
+    //temp variable to hold the line
+    std::string line;
+
+    //check if file is open
+    if (file.is_open()){
+
+        //get header line
+        std::getline(file, line);
+
+        //loop through the file
+        while (std::getline(file,line)) {
+            //vector to hold each song value
+            std::vector<std::string> songInfo;
+
+            //create stringstream
+            std::stringstream temp(line);
+
+            //variable to hold each value
+            std::string value;
+
+            //add each value to vector
+            while (getline(temp, value, ',')) {
+                songInfo.push_back(value);
+            }
+
+            //add song to class
+            songList.AddSong(songInfo);
+        }
+
+    } else {
+        //throw error if file was not opened
+        std::cout << "ERROR: FILE NOT OPENED" << std::endl;
+        return;
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //create Songs object
+    Songs songList;
+    parseFile("../Songs100.csv", songList);
+    std::cout << songList.GetAlbum("Testify") << std::endl;
     return 0;
 }
